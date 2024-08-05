7150aeec12a39e25b3c4a3f58d674c8cca6f2f03
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-08-05 13:33:56 +0000

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextPwa from 'next-pwa'
 
-export default nextConfig;
+const withPWA = nextPwa({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
+}
+
+const config = withPWA(nextConfig)
+
+export default config
