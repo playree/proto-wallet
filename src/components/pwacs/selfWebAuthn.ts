0b5e393ecb7ef5ce82dd5d1f3342438ab07a4567
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-08-14 05:40:23 +0000

    test

--- src/components/pwacs/selfWebAuthn.ts
+++ src/components/pwacs/selfWebAuthn.ts
@@ -1,3 +1,5 @@
+import base64url from 'base64url'
+
 import { cbor } from './cbor'
 
 export type RegistInfo = {
@@ -7,12 +9,42 @@ export type RegistInfo = {
   userDisplayName: string
 }
 
+const newDataView = (array: Uint8Array) => new DataView(array.buffer, array.byteOffset, array.byteLength)
+
+const coseToJwk = (cose: Uint8Array) => {
+  const coseObj = cbor.decode<{
+    '1': number
+    '3': number
+    '-1': Buffer
+    '-2': Buffer
+    '-3': Buffer
+  }>(cose)
+  console.debug('publicKey(CBOR):', coseObj)
+
+  if (coseObj[3] === -7) {
+    return {
+      kty: 'EC',
+      crv: 'P-256',
+      x: base64url(coseObj[-2]),
+      y: base64url(coseObj[-3]),
+    }
+  } else if (coseObj[3] === -257) {
+    return {
+      kty: 'RSA',
+      n: base64url(coseObj[-1]),
+      e: base64url(coseObj[-2]),
+    }
+  }
+  throw new Error('Unknown public key algorithm')
+}
+
 export const registWA = async ({ appName, appHost, userName, userDisplayName }: RegistInfo) => {
   const textDec = new TextDecoder()
   const textEnc = new TextEncoder()
 
   const keyid = crypto.getRandomValues(new Uint8Array(32))
   const challenge = crypto.getRandomValues(new Uint8Array(32))
+  console.debug('challenge:', base64url(Buffer.from(challenge)))
 
   const credential = await navigator.credentials.create({
     publicKey: {
@@ -33,6 +65,7 @@ export const registWA = async ({ appName, appHost, userName, userDisplayName }: 
     },
   })
 
+  // レスポンスを解析
   const authAttRes = (credential as PublicKeyCredential).response as AuthenticatorAttestationResponse
   const clientDataJSON: {
     challenge: string
@@ -42,10 +75,26 @@ export const registWA = async ({ appName, appHost, userName, userDisplayName }: 
   } = JSON.parse(textDec.decode(authAttRes.clientDataJSON))
   console.debug('clientDataJSON:', clientDataJSON)
 
-  const { authData } = cbor.decode<{ authData: Uint8Array }>(authAttRes.attestationObject)
+  // challengeの比較
+  if (clientDataJSON.challenge !== base64url(Buffer.from(challenge))) {
+    throw new Error('Invalid challenge')
+  }
+
+  // origin(domain)の比較
+  const originHost = new URL(clientDataJSON.origin).hostname
+  console.debug('originHost:', originHost)
+  if (originHost !== appHost) {
+    throw new Error('Invalid origin')
+  }
+
+  const { authData } = cbor.decode<{
+    fmt: string
+    attStmt: Record<string, unknown>
+    authData: Uint8Array
+  }>(authAttRes.attestationObject)
   console.debug('authData:', authData)
 
-  const authDataView = new DataView(authData.buffer, authData.byteOffset, authData.byteLength)
+  const authDataView = newDataView(authData)
   const credentialIdLength = authDataView.getUint16(53, false)
 
   const authDataObj = {
@@ -58,4 +107,14 @@ export const registWA = async ({ appName, appHost, userName, userDisplayName }: 
     credentialPublicKey: authData.slice(55 + credentialIdLength),
   }
   console.debug('authDataObj:', authDataObj)
+
+  // rpIdHashの比較
+  const rpIdHashReq = await crypto.subtle.digest('SHA-256', Buffer.from(appHost))
+  if (!Buffer.from(rpIdHashReq).equals(authDataObj.rpIdHash)) {
+    throw new Error('Invalid rpIdHash')
+  }
+
+  // 公開鍵取得
+  const publicKeyJwk = coseToJwk(authDataObj.credentialPublicKey)
+  console.debug('publicKeyJwk:', publicKeyJwk)
 }
